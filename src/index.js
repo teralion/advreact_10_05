4583d7e85d3bbb78ef73346f35ff6f4be65cf9de
teralion <slavad878@gmail.com>
2018-05-16 07:17:35 +0300

    Basic app configs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './App.js';
+import {ConnectedRouter} from 'react-router-redux';
 import registerServiceWorker from './registerServiceWorker';
+import history from './history';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store = {store}>
+        <ConnectedRouter history = {history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
